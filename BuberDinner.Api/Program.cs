6866349f0492339761179b84d97e8f51d84a6c76
Omar Betar <omar1betar@gmail.com>
2022-12-24 00:40:46 +0200

    ApplyAbstraction

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,6 +1,10 @@
+
+using BuberDinner.Application;
+
 var builder = WebApplication.CreateBuilder(args);
 
 {
+    builder.Services.AddApplication();
     builder.Services.AddControllers();
 
 }
---
+++ BuberDinner.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,12 @@
+using BuberDinner.Application.Authentication;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BuberDinner.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection services){
+        services.AddScoped<IAuthenticationService, AuthenticationService>();
+        return services;
+    }
+}
